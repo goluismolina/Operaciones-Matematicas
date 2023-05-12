42b048cfb1ebc1f7cd5edafe655b7f714bc7e449
Luis Molina <luisemr14molina@gmail.com>
2023-05-11 22:32:18 -0400

    first commit

---
+++ Index.js
@@ -0,0 +1,37 @@
+// Codigo que realiza operaciones matematicas basicas.
+
+// Operacion de suma
+
+function suma (){
+    resultado = 5 + 6
+    console.log(resultado)
+}
+suma()
+
+// Operacion resta.
+
+function resta(){
+    resultado = 5 - 6
+    console.log(resultado)
+}
+resta()
+
+//Operacion multiplicacion.
+
+function multiplicacion(){
+    resultado = 5 * 6
+    console.log(resultado)
+}
+multiplicacion()
+
+// Operacion division.
+
+function division(){
+    resultado = 6 / 5
+    console.log(resultado)
+}
+division()
+
+// Fin de las operaciones
+
+
